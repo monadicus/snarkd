2850fe880b3164c9192683132feae08c899c8ec6
0rphon <59403052+0rphon@users.noreply.github.com>
2022-11-30 16:23:36 -0800

    added fp6 & fp12 input generators

--- tools/test-generator/src/main.rs
+++ tools/test-generator/src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use rand::{thread_rng, Rng};
 use serde_json::Value;
-use snarkd_crypto::bls12_377::{Field, Fp, Fp2};
+use snarkd_crypto::bls12_377::{Field, Fp, Fp12, Fp2, Fp6};
 use std::{collections::BTreeMap, fs::write, path::PathBuf};
 use test_runner::{Case, TestConfig, TestExpectationMode};
 
@@ -33,6 +33,16 @@ enum Input {
     ThreeFp2,
     FourFp2,
     TwoFp2Lists,
+    OneFp6,
+    TwoFp6,
+    ThreeFp6,
+    FourFp6,
+    TwoFp6Lists,
+    OneFp12,
+    TwoFp12,
+    ThreeFp12,
+    FourFp12,
+    TwoFp12Lists,
 }
 
 impl Input {
@@ -48,6 +58,16 @@ impl Input {
             Input::ThreeFp2 => Self::gen_tests(n, Self::gen_fp2s::<3>),
             Input::FourFp2 => Self::gen_tests(n, Self::gen_fp2s::<4>),
             Input::TwoFp2Lists => Self::gen_tests(n, Self::gen_fp2_lists::<2>),
+            Input::OneFp6 => Self::gen_tests(n, Self::gen_fp6),
+            Input::TwoFp6 => Self::gen_tests(n, Self::gen_fp6s::<2>),
+            Input::ThreeFp6 => Self::gen_tests(n, Self::gen_fp6s::<3>),
+            Input::FourFp6 => Self::gen_tests(n, Self::gen_fp6s::<4>),
+            Input::TwoFp6Lists => Self::gen_tests(n, Self::gen_fp6_lists::<2>),
+            Input::OneFp12 => Self::gen_tests(n, Self::gen_fp12),
+            Input::TwoFp12 => Self::gen_tests(n, Self::gen_fp12s::<2>),
+            Input::ThreeFp12 => Self::gen_tests(n, Self::gen_fp12s::<3>),
+            Input::FourFp12 => Self::gen_tests(n, Self::gen_fp12s::<4>),
+            Input::TwoFp12Lists => Self::gen_tests(n, Self::gen_fp12_lists::<2>),
         }
     }
 
@@ -69,11 +89,14 @@ impl Input {
     }
 
     fn gen_lists<const N_ARGS: usize>(f: fn() -> Value) -> Value {
-        Value::Array(
-            (0..N_ARGS)
-                .map(|_| (0..thread_rng().gen_range(0..10)).map(|_| f()).collect())
-                .collect(),
-        )
+        (0..N_ARGS)
+            .map(|_| {
+                (0..thread_rng().gen_range(0..10))
+                    .map(|_| f())
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>()
+            .into()
     }
 
     fn gen_fp() -> Value {
@@ -81,7 +104,10 @@ impl Input {
     }
 
     fn gen_fps<const N_ARGS: usize>() -> Value {
-        Value::Array((0..N_ARGS).map(|_| Self::gen_fp()).collect())
+        (0..N_ARGS)
+            .map(|_| Self::gen_fp())
+            .collect::<Vec<_>>()
+            .into()
     }
 
     fn gen_fp_lists<const N_ARGS: usize>() -> Value {
@@ -90,15 +116,67 @@ impl Input {
 
     fn gen_fp2() -> Value {
         let v = Fp2::rand();
-        Value::Array(vec![v.c0.to_string().into(), v.c1.to_string().into()])
+        vec![v.c0.to_string(), v.c1.to_string()].into()
     }
 
     fn gen_fp2s<const N_ARGS: usize>() -> Value {
-        Value::Array((0..N_ARGS).map(|_| Self::gen_fp2()).collect())
+        (0..N_ARGS)
+            .map(|_| Self::gen_fp2())
+            .collect::<Vec<_>>()
+            .into()
     }
 
     fn gen_fp2_lists<const N_ARGS: usize>() -> Value {
         Self::gen_lists::<N_ARGS>(Self::gen_fp2)
+    }
+
+    fn gen_fp6() -> Value {
+        let v = Fp6::rand();
+        vec![
+            vec![v.c0.c0.to_string(), v.c0.c1.to_string()],
+            vec![v.c1.c0.to_string(), v.c1.c1.to_string()],
+            vec![v.c2.c0.to_string(), v.c2.c1.to_string()],
+        ]
+        .into()
+    }
+
+    fn gen_fp6s<const N_ARGS: usize>() -> Value {
+        (0..N_ARGS)
+            .map(|_| Self::gen_fp6())
+            .collect::<Vec<_>>()
+            .into()
+    }
+
+    fn gen_fp6_lists<const N_ARGS: usize>() -> Value {
+        Self::gen_lists::<N_ARGS>(Self::gen_fp6)
+    }
+
+    fn gen_fp12() -> Value {
+        let v = Fp12::rand();
+        vec![
+            vec![
+                vec![v.c0.c0.c0.to_string(), v.c0.c0.c1.to_string()],
+                vec![v.c0.c1.c0.to_string(), v.c0.c1.c1.to_string()],
+                vec![v.c0.c2.c0.to_string(), v.c0.c2.c1.to_string()],
+            ],
+            vec![
+                vec![v.c1.c0.c0.to_string(), v.c1.c0.c1.to_string()],
+                vec![v.c1.c1.c0.to_string(), v.c1.c1.c1.to_string()],
+                vec![v.c1.c2.c0.to_string(), v.c1.c2.c1.to_string()],
+            ],
+        ]
+        .into()
+    }
+
+    fn gen_fp12s<const N_ARGS: usize>() -> Value {
+        (0..N_ARGS)
+            .map(|_| Self::gen_fp12())
+            .collect::<Vec<_>>()
+            .into()
+    }
+
+    fn gen_fp12_lists<const N_ARGS: usize>() -> Value {
+        Self::gen_lists::<N_ARGS>(Self::gen_fp12)
     }
 }
 
